f205f81f4e2f05a57c0fe8cda0cb9ea90cacce63
unknown <lucas.200061@gmail.com>
2022-03-24 22:21:06 -0300

    miApi

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const port = 5000;
 const app = express();
 
 
+app.get("/",(req,res)=>{
+    res.send(`<h1> Hola estamos en mi API con express y NODE </h1>`);
+})
+
+
 app.listen(port,(err)=>{
     err? console.log("error") : console.log(`sv corriendo en http://localhost:${port}`);
 })
