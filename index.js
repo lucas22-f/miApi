0dba2bd9e35013e9db604395029770b21c78083b
unknown <lucas.200061@gmail.com>
2022-03-25 01:30:20 -0300

    miApi

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require("express");
-const port = 5000;
-const app = express();
 
+const express = require("express");
+const port = 3000;
+const app = express();
+const users = require("./data");
 
 app.get("/",(req,res)=>{
     res.send(`<h1> Hola estamos en mi API con express y NODE </h1>`);
+
+
 })
+//get all users
+app.get("/users",(req,res)=>{
+
+    if( users.length > 0){
+       return res.status(200).json(users);
+    }   
+    res.status(404).json({message:"resourse not found"}); 
+
+})
+// get user BY id 
+app.get("/users/:id",(req,res)=>{
+   
+    const userFound = users.find((user) => user.id === parseInt(req.params.id));
+    userFound ? res.status(202).json(userFound) : res.status(404).json({message :"error"});
+      
+})
+
 
 
 app.listen(port,(err)=>{
