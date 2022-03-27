5fa17876442bcc87219d115ae8e806c36a9c42cf
unknown <lucas.200061@gmail.com>
2022-03-27 18:11:53 -0300

    27-3

--- index.js
+++ index.js
@@ -8,37 +8,41 @@ app.get("/", (req, res) => {
 });
 //get all users
 app.get("/users", (req, res) => {
+
+
     if (users.length > 0) {
         return res.status(200).json(users);
+
     }
     res.status(404).json({ message: "resourse not found" });
+
+
 });
 // get user BY id
 app.get("/users/:id", (req, res) => {
+
+
     if (isNaN(Number(req.params.id))) {
-        return res
-            .status(400)
-            .json({ message: "debe ser un numero entero positivo cracken 😎" });
+
+        return res.status(400).json({ message: "debe ser un numero entero positivo cracken 😎" });
+
     }
 
     const userFound = users.find((user) => user.id === parseInt(req.params.id));
-    userFound
-        ? res.status(202).json(userFound)
-        : res.status(404).json({ message: "error" });
+
+    userFound? res.status(202).json(userFound): res.status(404).json({ message: "error" });
+
+
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/users", (req, res) => {
     const { name, username, email } = req.body;
-    if (
-        !name ||
-        !username ||
-        (!email && !name === "") ||
-        username === "" ||
-        email === ""
-    ) {
+    if (!name ||!username ||(!email && !name === "") ||username === "" ||email === ""){
+
         res.status(400).json({ message: "all fields required" });
+
     } else {
         console.log(req.body);
         users.push({ ...req.body });
@@ -48,21 +52,19 @@ app.post("/users", (req, res) => {
 //delete user by id
 app.delete("/users/:id", (req, res, next) => {
     if (isNaN(Number(req.params.id)) || req.params.id < 1) {
-        return res.status(400).json({ message: "ID debe ser un número entero positivo mayor que 0" })
+        return res.status(400).json({ message: "ID debe ser un número entero positivo mayor que 0" });
     }
     if (users.find((user) => user.id === Number(req.params.id))) {
-        let filteredArr = users.filter((user) => user.id !== Number(req.params.id))
-        users = filteredArr
-        res.status(200).json({ message: "Resource deleted" })
+        let filteredArr = users.filter((user) => user.id !== Number(req.params.id));
+        users = filteredArr;
+        res.status(200).json({ message: "Resource deleted" });
     } else {
-        next()
+        next();
     }
-})
+});
 
 app.listen(port, (err) => {
-    err
-        ? console.log("error")
-        : console.log(`sv corriendo en http://localhost:${port}`);
+    err? console.log("error"): console.log(`sv corriendo en http://localhost:${port}`);
 });
 
 app.use((req, res) => {
